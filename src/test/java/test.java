df6dd75cc07d3fe89508bc4eee7ea86611683e28
RaulHenri <yungrah44@gmail.com>
2025-02-17 21:47:17 -0500

    person 2 commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,7 +2,7 @@ public class test {
 
     public static void main(String[] args) {
 
-        System.out.println("This is the project Version 5");
+        System.out.println("This is the project Version 6 made by person 2");
     }
 }
-// done with this version five
+// done with this version six
